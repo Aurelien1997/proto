82db1afcec87c93f3cfcc61d221e1a5aff5e813e
Aurelien1997 <tawpo36@gmail.com>
2022-05-19 23:06:16 +0100

    route

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
 import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from './Home';
+import About from './About';
+import Works from './Works';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Navbar />
-        <div className='content'>
-        <Routes>
-            <Route path="/a" component={<Home />} />
-        </Routes>
+        <div className="content">
+          <Routes>
+            <Route path='/' element={<Home />} /> 
+            <Route path='/About' element={<About />} />
+            <Route path='/Works' element={<Works />} />
+          </Routes>
         </div>
       </div>
     </Router>
---
+++ src/Works.js
@@ -0,0 +1,11 @@
+const Works = () => {
+  return (
+    <p>
+      ### Au fil des années, nous avons pu accompagner les meilleurs.
+
+      Découvrez pas à pas comment nous avons été présent pour lancer vos marques préférées.
+    </p>
+  );
+}
+ 
+export default Works;
